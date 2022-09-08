dec7d474eb7e31ddc29d209ce1594fab34ff5a00
Bernadette Christiner <b.christiner93@gmail.com>
2022-09-08 17:53:53 +0200

    Implement function to create table, and save data

--- SIWeekManager.js
+++ SIWeekManager.js
@@ -1,35 +1,92 @@
-const chosenSiWeek = document.getElementById("si-week");
+const chosenSiWeek = document.getElementById("select-si-week");
 const investedHours = document.getElementById("hours");
 const dateControl = document.getElementById("date");
+const investedHoursOutput = document.querySelector(".invested-hours");
 const displayDetailsButton = document.getElementById("displayDetails");
-let SIWEEKDATA = [];
+const displayModul2 = document.querySelector(".modul2");
+const table = document.getElementById("table");
+let hideSiWeekSection = document.querySelector(".hide");
+let dataTable = document.getElementById("data-table");
+let tableHeader = document.querySelector(".tableHeader-day");
 
-function setSIWeekDataToLocalStorage(event) {
-  const data = {
-    hours: investedHours.value,
-    date: dateControl.value,
-    siWeek: chosenSiWeek.value,
-  };
+let SIWEEKDATA;
+let hoursCollection = [];
 
-  SIWEEKDATA.push(data);
-  localStorage.setItem("data", JSON.stringify(SIWEEKDATA));
-  chosenSiWeek.value = "Choose SI-Week";
+//check error document is not defined
+if (typeof window !== "undefined") {
+  console.log("You are on the browser");
+} else {
+  console.log("You are on the server");
 }
 
-chosenSiWeek.addEventListener("change", setSIWeekDataToLocalStorage);
-
-displayDetailsButton.addEventListener("click", () => {
+window.addEventListener("load", () => {
   SIWEEKDATA = JSON.parse(localStorage.getItem("data"));
-  const table = document.getElementById("table");
-  const tableRow = document.createElement("tr");
-  const cellDay = document.createElement("td");
-  const cellHours = document.createElement("td");
+
+  if (SIWEEKDATA === null) {
+    SIWEEKDATA = [];
+  } else {
+    SIWEEKDATA = JSON.parse(localStorage.getItem("data"));
+    investedHoursOutput.innerHTML = localStorage.getItem("investedHours");
+  }
+
+  chosenSiWeek.addEventListener("change", (event) => {
+    const data = {
+      hours: investedHours.value,
+      date: dateControl.value,
+      siWeek: chosenSiWeek.value,
+    };
+
+    if (event.target.value === "1") {
+      SIWEEKDATA.push(data);
+      localStorage.setItem("data", JSON.stringify(SIWEEKDATA));
+      drawTable();
+    }
+
+    let select_box = document.getElementById("select-si-week");
+    select_box.selectedIndex = "Choose SI-Week";
+    investedHours.value = "";
+
+    sumInvestedHours();
+    // location.reload();
+  });
+  displayModul2.addEventListener("click", () => {
+    hideSiWeekSection.classList.toggle("hide");
+  });
+
+  drawTable();
+});
+
+function sumInvestedHours() {
+  let hours = document.getElementsByClassName("hours");
+  let collectionOFHours = [];
+  let result = 0;
+  for (let hour of hours) {
+    console.log(hour);
+    collectionOFHours.push(+hour.innerHTML);
+  }
+  for (let hour of collectionOFHours) {
+    result = result + hour;
+  }
+  console.log(result);
+  localStorage.setItem("investedHours", JSON.stringify(result));
+  investedHoursOutput.innerHTML = localStorage.getItem("investedHours");
+}
+function drawTable() {
+  table.innerHTML = "";
 
   for (let data of SIWEEKDATA) {
+    const tableRow = document.createElement("tr");
+    let cellDay = document.createElement("td");
+    let cellHours = document.createElement("td");
     table.appendChild(tableRow);
     tableRow.appendChild(cellDay);
     tableRow.appendChild(cellHours);
     cellDay.innerHTML = data.date;
+    cellDay.setAttribute("class", "day");
     cellHours.innerHTML = data.hours;
+    cellHours.setAttribute("class", "hours");
   }
+}
+displayDetailsButton.addEventListener("click", () => {
+  table.classList.toggle("hide");
 });
